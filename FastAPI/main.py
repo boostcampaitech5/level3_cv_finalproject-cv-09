1ffaa864a7e9a1fcecc17e4615d702f930915c57
ygh1254 <ygh1254@hanyang.ac.kr>
2023-07-20 01:50:01 +0000

    refactoring: support png type

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -26,19 +26,6 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 @app.on_event("startup")
 async def startup_event():
-    app.state.colors = [
-        (0, 0, 0),
-        (0.8196078431372549, 0.2901960784313726, 0.25882352941176473),
-        (0.42745098039215684, 0.9490196078431372, 0.2),
-        (0.9490196078431372, 0.9254901960784314, 0.8862745098039215),
-        (0.5764705882352941, 0.19607843137254902, 0.6235294117647059),
-        (0.0196078431372549, 0.41568627450980394, 0.9725490196078431),
-        (0.3764705882352941, 0.20784313725490197, 0.09411764705882353),
-        (0.12156862745098039, 0.4745098039215686, 0.38823529411764707),
-        (0.00392156862745098, 0.34901960784313724, 0.01568627450980392),
-        (0.4470588235294118, 0.00392156862745098, 0.03137254901960784),
-        (0.32941176470588235, 0.34901960784313724, 0.7607843137254902),
-    ]
 
     # Load the pre-trained model
     sam_checkpoint = "weights/mobile_sam.pt"
@@ -53,6 +40,12 @@ async def startup_event():
     
     # Lang-SAM load
     app.state.lang_sam = LangSAM(sam_type="vit_h", device = device)
+
+def change_path(path):
+    if path.endswith('.png'):
+        path = str(path.split('.')[0] + '.png')
+    return path
+        
 
 def rle_encode(mask):
     """
@@ -158,16 +151,10 @@ async def zip_upload(id: str = Form(...), files: UploadFile = File(...)):
 
 @app.post("/segment/")
 async def segment(path: str = Form(...)):
+    path = change_path(path)
     id, file_name = path.split("/")
-    # Change file name from a.png to a.jpg as file is converted to jpg
-    if file_name.endswith('.png'):
-        file_name = str(file_name.split('.')[0]) + '.jpg'
     img_path = f"{FOLDER_DIR}/{id}/original/{file_name}"
     img = Image.open(img_path).convert("RGB")
-    if file_name.endswith(".png"):
-        jpg_path = f"{file_name.split('.')[0]}.jpg"
-        img.save(jpg_path)
-        img = Image.open(jpg_path)
     output = await segment_everything(img)
     output = output.convert("RGB")
     if not os.path.isdir(f"{FOLDER_DIR}/{id}/segment/"):
@@ -177,29 +164,16 @@ async def segment(path: str = Form(...)):
         f"{FOLDER_DIR}/{id}/segment/{file_name}",
         media_type="image/jpg",
     )
-    if file_name.endswith(".png"):
-        seg_img = FileResponse(
-            f"{FOLDER_DIR}/{id}/segment/{file_name}",
-            media_type="image/png",
-        )
     return seg_img
 
 
 @app.post("/segment_text/")
 async def segment_text(path: str = Form(...), text_prompt: str = Form(...)):
     box_threshold, text_threshold = 0.3, 0.3
-
-    text_prompt = text_prompt.replace(",", ".")
+    path = change_path(path)
     id, file_name = path.split("/")
-    # Change file name from a.png to a.jpg as file is converted to jpg
-    if file_name.endswith('.png'):
-        file_name = str(file_name.split('.')[0]) + '.jpg'
     img_path = f"{FOLDER_DIR}/{id}/original/{file_name}"
-    if file_name.endswith(".png"):
-        jpg_path = f"{file_name.split('.')[0]}.jpg"
-        img = Image.open(img_path).convert("RGB")
-        img.save(jpg_path)
-        img_path = jpg_path
+    text_prompt = text_prompt.replace(",", ".")
     text_seg_dict, segmented_image = await segment_dino(box_threshold, text_threshold, img_path, text_prompt = text_prompt)
     if not os.path.isdir(f"{FOLDER_DIR}/{id}/segment/"):
         os.mkdir(f"{FOLDER_DIR}/{id}/segment/")
@@ -216,8 +190,7 @@ async def segment_text(path: str = Form(...), text_prompt: str = Form(...)):
 @app.post("/json_download/")
 def json_download(path: str = Form(...)):
     id, file_name = path.split("/")
-    if file_name.endswith('.png'):
-        file_name = str(file_name.split('.')[0]) + '.jpg'
+    path = change_path(path)
     file_name = file_name.split(".")[0]
     output = {"test": [1, 2, 3, 4], "test2": [5, 6, 7, 8]}
     with open(f"{FOLDER_DIR}/{id}/{file_name}_segment.json", "w") as f:
